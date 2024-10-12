719a8143335a5a900527ef3cd544c5c7ac9968ec
golon222 <kuba.golon22@gmail.com>
2024-10-13 00:50:01 +0200

    Update slide.js

--- slide.js
+++ slide.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
         slidesToShow: 1, 
         slidesToScroll: 1,
         arrows: true,    
-        prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',  
-        nextArrow: '<button type="button" class="slick-next">&#10095;</button>'   
+
     });
 });
